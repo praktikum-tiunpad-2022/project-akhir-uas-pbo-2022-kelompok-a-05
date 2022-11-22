2db420f0db106c4c978ab4f80bc4600252dcd1f7
zakianoor <86943025+zakianoor@users.noreply.github.com>
2022-11-22 11:17:36 +0700

    Update Squares.java

--- src/Squares.java
+++ src/Squares.java
@@ -1,15 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 import java.awt.*;
 import javax.swing.*;
 import javax.swing.border.*;
-/**
- *
- * @author zaqia
- */
+
 public class Squares extends JPanel {
     public final int CELL_COUNT = 9;
     public Cell [] cells = new Cell[CELL_COUNT];
